07043b95e716c30795c91b542d561099fc674bd4
AndreKCoding <131627515+AndreKCoding@users.noreply.github.com>
2023-07-08 15:29:17 -0300

    Add files via upload

---
+++ impressora.js
@@ -0,0 +1,75 @@
+img = "";
+stats = "";
+
+objects = [];
+
+function setup()
+{
+    canvas = createCanvas(640, 400);
+    canvas.position(350, 210);
+
+    detectorObject = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objeto";
+}
+
+function preload()
+{
+    img = loadImage('Impressor.jpeg');
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    
+    if(stats != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Objeto Detectado";
+            document.getElementById("quantidade").innerHTML = "Foram detectados " + objects.length + " Objetos";
+            fill('red');
+            porcentagem = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + porcentagem + "%", objects[i].x, objects[i].y + 20);
+            noFill();
+            stroke('red');
+            rect(objects[i].x, objects[i].y + 20, objects[i].width + 100, objects[i].height + 100);
+        }
+    }
+    else
+    {
+        document.getElementById("status").innerHTML = "Objeto Não Detectado";
+    }
+
+}
+
+function start()
+{
+    detectorObject = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objeto";
+}
+
+function modelLoaded()
+{
+    console.log("O Modelo foi Carregado!");
+    stats = true;
+    detectorObject.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+
+    objects = results;
+
+    floor(objects);
+    console.log(objects);
+}
+
+function voltar()
+{
+    location.replace('index.html');
+}
---
+++ main.js
@@ -0,0 +1,29 @@
+function frutas()
+{
+    location.replace('frutas.html');
+}
+
+function computador()
+{
+    location.replace('computador.html');
+}
+
+function televisao()
+{
+    location.replace('televisao.html');
+}
+
+function ventilador()
+{
+    location.replace('ventilador.html');
+}
+
+function impressora()
+{
+    location.replace('impressora.html');
+}
+
+function guardaRoupa()
+{
+    location.replace('guarda.html');
+}
